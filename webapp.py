7d3b978e6a99155f14cb889c004f21b43bc6aa16
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-04-19 00:58:28 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -102,7 +102,7 @@ This app counts the disease probability of query CXR(Chest XRay Frontal-Scan)!
 st.image("https://img.shields.io/badge/Version-Alpha-orange.svg")
 
 st.info("""CheXM is a deep learning model.The model is trained on the Chest X-ray images of a revised version of [ Paul Mooney's most popular Dataset Published in Cell Journal](https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia).""")
-
+st.info("DOI:https://doi.org/10.1016/j.cell.2018.02.010")
   
 ######Uploader########
 
