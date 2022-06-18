91c567e4e361f82b77961f101fee06f5c0389c54
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-19 00:54:40 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -208,7 +208,7 @@ plt.show()
 st.title("Visuals")
 
 for i in labels:
-        x = np.array([i[0:]])
+        x = np.array([i[1]],axis=1)
         mylabels = [i[:]]
 
         fig = plt.figure(figsize=(10, 4))
