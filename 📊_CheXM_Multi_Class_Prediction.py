a1f679dfa404722ea8033f0fa6475c0d723d29f8
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-19 00:20:03 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -223,3 +223,9 @@ plt.scatter(x, labels= mylabels)
     
 st.balloons()
 st.pyplot(fig)
+
+chart_data = pd.DataFrame(
+    np.random.randn(50, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
