81d166457643c760fa2cd370efb28bd62af530b8
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-18 19:24:36 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -52,6 +52,7 @@ with col1:
     col1.xray = st.file_uploader("Upload an image", type = ["jpg", "png","jpeg"])
 
 
+components.iframe("https://giphy.com/embed/WfZBqVVywQdd1OloEd", width=300, height=300)
 
 
 with col2:
@@ -121,7 +122,6 @@ if col1.xray is not None:
     for i in labels:
         st.write("Prediction", i[0], ",  Confidence Score: ", i[1])
 
-st.sidebar.image("https://www.pngall.com/wp-content/uploads/6/X-Ray-PNG-Picture.png", width=100)
 
 
 
@@ -142,6 +142,8 @@ lottie_t1 = load_lottieurl(lottie_url_t1)
 
 with st.sidebar:
     st_lottie(lottie_t1, width=199, height=150)
+    st_lottie(lottie_ball)
+
 
 st.sidebar.title("CheXM Multi Class Prediction on CXR Scans")
 st.sidebar.subheader("MultiClass Prediction ")
@@ -149,7 +151,6 @@ st.sidebar.markdown("---")
 st.sidebar.subheader("#Alpha Version Under Development")
 st.sidebar.image("https://img.shields.io/badge/CheXM-v1.0-green.svg", width=100)
 
-components.iframe("https://giphy.com/embed/WfZBqVVywQdd1OloEd", width=300, height=300)
 
 st.sidebar.subheader("About")
 st.sidebar.info("CheXM is a deep learning model, It is a convolutional neural network (ResNet50) model that is trained to detect pneumonia in chest X-ray images. This model is trained on 18 Classes CXR X-ray images,  This tool is based on various training datasets  was published by [Paulo Breviglieri](https://www.kaggle.com/datasets/pcbreviglieri/pneumonia-xray-images), a revised version of [ Paul Mooney's most popular Dataset Published in Cell Journal](https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia).")
@@ -171,8 +172,7 @@ st.sidebar.markdown(
     )
 
 ####lottie logo####
-st_lottie(lottie_ball)
-
+    
 
 #####LOTTIE####
 
