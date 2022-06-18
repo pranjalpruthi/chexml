4ac8b791e92af1246afd37bc15524ff1f70aad55
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-18 21:42:43 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -185,6 +185,13 @@ with st.sidebar:
 
 ########lottie############
 
+import plotly.express as px
+data_canada = px.data.gapminder().query("country == 'Canada'")
+fig = px.bar(data_canada, x='year', y='pop')
+fig.show()
+
+
+
 labels = predict(col1.xray)
 for i in labels:
         Prediction= i[0]
