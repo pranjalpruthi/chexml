b3466bfa18986a385e1bc7fe21b1172a19d88848
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-18 19:44:44 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -23,6 +23,10 @@ import torchxrayvision as xrv
 import requests
 import streamlit.components.v1 as components
 from streamlit_lottie import st_lottie
+import plotly.figure_factory as ff
+import numpy as np
+
+
 
 
 
@@ -179,3 +183,23 @@ with st.sidebar:
 
 
 ########lottie############
+
+def load_data():
+    # Add histogram data
+    x1 = np.random.randn(200) - 2
+    x2 = np.random.randn(200)
+    x3 = np.random.randn(200) + 2
+
+    # Group data together
+    hist_data = [x1, x2, x3]
+    return hist_data
+
+hist_data = load_data()
+
+group_labels = ["Group 1", "Group 2", "Group 3"]
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
+
+# Plot!
+st.plotly_chart(fig)
