5c96c31f7519a2704cf92a016ed9a25232c75694
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-18 22:42:27 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -205,7 +205,7 @@ plt.show()
 
 for i in labels:
         x = np.array([i[1]])
-        mylabels = i[0]
+        mylabels = [i[0]]
 
         fig = plt.figure(figsize=(18, 18))
         plt.pie(x, labels = mylabels)
