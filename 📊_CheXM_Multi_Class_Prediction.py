de0d2fa4d27caf227472f2ffc04f741f5f8ec761
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-18 21:33:45 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -185,22 +185,14 @@ with st.sidebar:
 
 ########lottie############
 
-def load_data():
-    # Add histogram data
-    x1 = np.random.randn(200) - 2
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2
 
-    # Group data together
-    hist_data = [x1, x2, x3]
-    return hist_data
 
-hist_data = load_data()
+Prediction= i[0]
 
-group_labels = ["Group 1", "Group 2", "Group 3"]
+Confidence_Score = i[1]
 
 # Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
+fig = ff.create_distplot(Prediction, Confidence_Score, bin_size=[0.5])
 
 # Plot!
 st.plotly_chart(fig, use_container_width=True)
