83bc7ceeb027e169c2e1bed11c1f4ff2b96efb71
Pranjal Pruthi <pranjalpruthi@gmail.com>
2022-06-19 18:08:15 +0530

    Update 📊_CheXM_Multi_Class_Prediction.py

--- 📊_CheXM_Multi_Class_Prediction.py
+++ 📊_CheXM_Multi_Class_Prediction.py
@@ -105,7 +105,9 @@ def predict(image):
     transform = transforms.Compose([
     transforms.Resize(512),
     transforms.ToTensor(),
-    transforms.Normalize((0.5), (0.5)),
+    transforms.Normalize(mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225]
+        ),
     transforms.Grayscale(num_output_channels=1)
     ])
 
